ce8c08939ca91f98f4013c450c8a07ef9eb6ce92
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2020-10-29 22:35:20 +0700

    изменен файл main

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-import math, numpy, random
-b = 0
+import math, random
+b, r = 0, 0
+
+
 def sigmoid(x):
-    return 1/(1 + math.exp(-x))
+    return 1 / (1 + math.exp(-x))
 
-inputs = [0, 1, 1]
+inputs = [0, 1, 0]
 
-weights = [10575, 10575, 10575]
 
-"""weights = numpy.random.random((3, 1)) 
-
+weights = [random.random() for i in range(len(inputs))]  # weights = [21151, 21151, 21151]
 
 for i in range(200000):
     outputs = sigmoid(1)
     e = 1 - outputs
-    r = e *(outputs *e)
-    weights += r
-    i += 1
+    r = e * (outputs * e)
+    r += r
 
-print(weights)"""
+for i in range(len(weights)):
+    weights[i] += r * 200000
+print(weights)
+
 
 for i in range(len(inputs)):
     a = (inputs[i] * weights[i])
     b += a
-    i += 1
 
 print(math.floor(sigmoid(b)))
