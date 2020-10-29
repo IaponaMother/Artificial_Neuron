1cace4b6dce51e6d76ee6f6beac44020dcf241cf
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2020-10-29 21:12:30 +0700

    изменен файл main

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-import numpy
-
+import math, numpy, random
+b = 0
 def sigmoid(x):
-    return 1/(1 + numpy.exp(-x))
+    return 1/(1 + math.exp(-x))
 
-training_inputs = numpy.array([
-    [0, 0, 1],
-    [1, 1, 1],
-    [1, 0, 1],
-    [0, 1, 1]])
+inputs = [0, 1, 1]
 
-training_outputs = numpy.array([[0, 1, 1, 0]]).T
+weights = [10575, 10575, 10575]
 
-numpy.random.seed(1)
+"""weights = numpy.random.random((3, 1)) 
 
-weights = (13, -0.2, -6.14)
 
-"""print("случайные веса: ")
-weights = 2 * numpy.random.random((3, 1)) -1
+for i in range(200000):
+    outputs = sigmoid(1)
+    e = 1 - outputs
+    r = e *(outputs *e)
+    weights += r
+    i += 1
+
 print(weights)"""
 
+for i in range(len(inputs)):
+    a = (inputs[i] * weights[i])
+    b += a
+    i += 1
 
-"""for i in range(200000):
-    input_layer = training_inputs
-    outputs = sigmoid(numpy.dot(input_layer, weights))
-
-    err = training_outputs - outputs
-    adjustments = numpy.dot(input_layer.T, err *(outputs *(1 - outputs)))
-    weights += adjustments
-
-print("Веса после обучения: ")
-print(weights)"""
-
-print("результат после обучения: ")
-input_layer = training_inputs
-outputs = sigmoid(numpy.dot(input_layer, weights))
-print(outputs)
+print(math.floor(sigmoid(b)))
