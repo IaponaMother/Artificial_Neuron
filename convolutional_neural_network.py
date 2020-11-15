8b3b9feb7812d4a078bf382d9872db8760807b34
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2020-11-15 09:12:00 +0700

    добавлен файл

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -1,15 +1,17 @@
 import random
 list1, list2, result = [], [], []
 
-for i in range(4):  
+for i in range(4):
     list1.append([])
     for j in range(4):
         list1[i].append(random.randint(0, 10))
-print(list1)
+for i in list1:
+    print(i)
+
 
 for i in range(4):
     list2.append(random.randint(1, 3))
-print(list2)
+
 
 for i in range(4):
     a = 0
