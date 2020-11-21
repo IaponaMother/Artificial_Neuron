ee4a8dbe91f701082e3fb2b4ab9afeb6b87da0b3
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2020-11-21 21:46:16 +0700

    изменен файл convolutional_neural_network

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -3,34 +3,51 @@ import random
 input_list = []
 filter = []
 filter2 = []
-result = []
+multiplied_matrices = []
 list = []
 r = 0
+
 
 for i in range(4):  # создание первой матрицы
     input_list.append([])
     for j in range(4):
-        input_list[i].append(random.randint(0, 3))
-print(input_list)
+        input_list[i].append(random.randint(0, 10))
 
-for i in range(2):  # создание второй матрицы
+for i in input_list:  # вывод первой матрицы
+    str1 = " "
+    for il in i:
+        str1 += str(il) + " "
+    print(str1)
+
+
+for i in range(2):  # создание второй матрицы(фильтра)
     filter.append([])
     for j in range(2):
-        filter[i].append(random.randint(0, 3))
+        filter[i].append(random.randint(0, 1))
 
+print("\n")
+for i in filter:  # вывод второй матрицы
+    str1 = " "
+    for il in i:
+        str1 += str(il) + " "
+    print(str1)
 
 for i in filter:
-    filter2.extend(i)
-filter2 = [filter2] * 4
-print("\n", filter2)
+    a = i * 2
+    filter2.append(a)
+filter2 = filter2 * 2
 
 
-for il in range(4):  # перемножение матриц
-    for j in range(4):
-        for i in range(4):
-            r = r + input_list[il][i] * filter2[i][j]
+for i in range(4):  # перемножение элементов матриц
+    for il in range(4):
+        r = input_list[i][il] * filter2[i][il]
         list.append(r)
-        r = 0
-    result.append(list)
+    multiplied_matrices.append(list)
     list = []
-print("\n", result)
+
+print("\n")
+for i in multiplied_matrices:  # вывод результата
+    str1 = " "
+    for il in i:
+        str1 += str(il) + " "
+    print(str1)
